7fe64d7c8814bdf876dbba77ade0cff7671e32fa
Ashunti Ware <ashunti@Ashuntis-Air.attlocal.net>
2022-11-13 19:21:18 -0600

    first commit

---
+++ w3d3functions.js
@@ -0,0 +1,66 @@
+/* 
+1. Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
+*/
+
+const maxOfTwoNumbers = (10, 20) => {
+    if(10<20){
+        return 20;
+    }
+  };
+  
+  
+  /*
+  2. Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+  */
+  const maxOfThree = (5, 10, 20) => {
+    if(5 < 10 < 20) {
+      return Math.max(5, 10, 20);
+    }  
+    };
+  
+  /*
+  3. Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+  */
+  const isCharacterAVowel = (b) => {
+    let vowels = ['a','e','i','o','u'];
+    if (vowels.indexOf(b)) !==1){
+      return true; 
+    }
+    return false;
+  };
+  
+  /*
+  4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+  */
+  
+  const sumArray = (arr) => {
+    let sum = 0
+    for (let )
+  };
+  
+  /*
+  5.Write a function that returns the number of arguments passed to the function when called.
+  */
+  
+  /*
+  6. Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+  */
+  
+  reverseString = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  7. Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+  */
+  findLongestWord = () => {
+    // ADD YOUR CODE HERE
+  };
+  
+  /*
+  8. Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
+  */
+  filterLongWords = () => {
+    // ADD YOUR CODE HERE
+  };
+  
